4e196d19e159dcb2cf6c6aa162c968d80015f095
Vinícius Guasso <viniguasso10@gmail.com>
2022-04-05 10:50:34 -0300

    corrigido

--- Aula/avaliacao01/Frases.java
+++ Aula/avaliacao01/Frases.java
@@ -1,6 +1,5 @@
 import javax.swing.JOptionPane;
 public class Frases {
-
     public static String solicitarFrase(String frase) {
         return JOptionPane.showInputDialog(frase);
     }
@@ -8,16 +7,15 @@ public class Frases {
         int ponto = Integer.parseInt(JOptionPane.showInputDialog("Sua frase contém "+ tamanho +" carácteres.\nDigite o ponto inicial da extração da frase: "));
         JOptionPane.showMessageDialog(null, "A sua frase extraída a partir do "+ ponto +"º caráctere: "+ frase.substring(ponto, tamanho));
     }
-    public static void gerarCaractere(String frase, int tamanho) {
-        int caractere = (int) (Math.random() * tamanho);
-        JOptionPane.showMessageDialog(null, "Número aleatório gerado pelo Computador: "+ caractere);
-        JOptionPane.showMessageDialog(null, "O caráctere extraída da "+ caractere +"º posição: "+ frase.charAt(caractere));
+    public static void gerarMostrarCaractere(String frase, int tamanho) {
+        int caractere = (int) (Math.random() * tamanho + 1);
+        JOptionPane.showMessageDialog(null, "Posição aleatória gerada pelo Computador: "+ caractere +"º");
+        JOptionPane.showMessageDialog(null, "O caráctere extraído da "+ caractere +"º posição: "+ frase.charAt(caractere - 1));
     }
-
     public static void main (String[] args) {
         String frase = solicitarFrase("Digite sua frase: ");
         int tamanho = frase.length();
         cortarFrase(frase, tamanho);
-        gerarCaractere(frase, tamanho);
+        gerarMostrarCaractere(frase, tamanho);
     }
 }
